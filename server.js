395e82e06eb43ffb7e8934ec0094b9c2abfa11e6
vakatigireesh <59114051+vakatigireesh@users.noreply.github.com>
2021-10-12 13:49:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Gireesh, Hope you are doing well !!');
+  res.send('Hello Gireesh and Sandhesh, Hope you are doing well !!');
 });
 
 app.listen(PORT, HOST);
